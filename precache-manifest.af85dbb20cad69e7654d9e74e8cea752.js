e60f505bd02b70f1b0665722f4f8b5acfc95a348
Wilson Lam <lin-1987@live.com>
2020-07-21 17:25:18 -0700

    Updates

---
+++ precache-manifest.af85dbb20cad69e7654d9e74e8cea752.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff551228340abcc5a0035365752731b",
+    "url": "/Student-Portfolio-Test/index.html"
+  },
+  {
+    "revision": "f4bcf2fb96030bf94a04",
+    "url": "/Student-Portfolio-Test/static/css/main.adf59f7a.chunk.css"
+  },
+  {
+    "revision": "c1e23bfc2ce36a57b0f8",
+    "url": "/Student-Portfolio-Test/static/js/2.46c2815b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Student-Portfolio-Test/static/js/2.46c2815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bcf2fb96030bf94a04",
+    "url": "/Student-Portfolio-Test/static/js/main.ae20ff13.chunk.js"
+  },
+  {
+    "revision": "9a183644d9e00fcbcb5e",
+    "url": "/Student-Portfolio-Test/static/js/runtime-main.ace8e909.js"
+  },
+  {
+    "revision": "8c68d0778b734fca6f6b710b8a914c62",
+    "url": "/Student-Portfolio-Test/static/media/Burgerlogger.8c68d077.png"
+  },
+  {
+    "revision": "51279603c7672228d0f216dadcf54010",
+    "url": "/Student-Portfolio-Test/static/media/S7LT1i.51279603.jpg"
+  },
+  {
+    "revision": "139dd497d53f9849fd63f9d5fccf9cbd",
+    "url": "/Student-Portfolio-Test/static/media/YouMDb.139dd497.png"
+  },
+  {
+    "revision": "aafc75fb820701e76b0394d195d31560",
+    "url": "/Student-Portfolio-Test/static/media/aboutme.aafc75fb.jpg"
+  },
+  {
+    "revision": "10807f6757ba754346310c6e85d5c2fd",
+    "url": "/Student-Portfolio-Test/static/media/outerspacehero.10807f67.jpeg"
+  },
+  {
+    "revision": "46a849f52f4a15a3b7f2884e36f5b752",
+    "url": "/Student-Portfolio-Test/static/media/spacehubthumbnail.46a849f5.png"
+  },
+  {
+    "revision": "51a5a4f0e29b910e8ed7e2ebb746f93c",
+    "url": "/Student-Portfolio-Test/static/media/thievesmonopoly.51a5a4f0.png"
+  },
+  {
+    "revision": "9a9955e0f524137b27523cce3cf43756",
+    "url": "/Student-Portfolio-Test/static/media/weather.9a9955e0.png"
+  },
+  {
+    "revision": "c6e1137707a83cabddc5f4e126c70d81",
+    "url": "/Student-Portfolio-Test/static/media/workday.c6e11377.png"
+  }
+]);
