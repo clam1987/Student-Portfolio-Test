d0125527824c3de8263dfa10838f360a41819ebf
Wilson Lam <lin-1987@live.com>
2020-07-21 17:31:29 -0700

    test2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       
     <div className="App">
             <Route exact path="/" component={LandingPage} />
